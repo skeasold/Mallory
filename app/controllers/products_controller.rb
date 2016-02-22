baaecc3adda7d57c0501c88d72f4b81ab1116a42
Skylar King <sking@crazyawesome.io>
2016-02-21 21:16:55 -0600

    Add Bootstrap. Add discounts and availability. Needs styling, categories, and warehouse.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,23 +3,22 @@ require 'csv'
 class ProductsController < ApplicationController
 
   def list
-    @products = fetch_products
-    @available_products = @products.select { |product| product.quantity.to_i > 0 }
+    @products = instock_clearance
   end
 
   def view
-    @products = fetch_products
-    @product = @products.find { |product| product.pid == params[:pid] }
+    products = instock_clearance
+    @product = products.find { |product| product.pid == params[:pid] }
   end
 
-  def fetch_products
-    @products = []
+  def products_import
+    products = []
     CSV.foreach("mf_inventory.csv", headers: true) do |row|
       product_hash = row.to_hash
 
       product = Product.new
       product.pid = product_hash["pid"]
-      product.item = product_hash["item"]
+      product.name = product_hash["item"]
       product.description = product_hash["description"]
       product.price = product_hash["price"]
       product.condition = product_hash["condition"]
@@ -29,8 +28,17 @@ class ProductsController < ApplicationController
       product.img_file = product_hash["img_file"]
       product.quantity = product_hash["quantity"]
       product.category = product_hash["category"]
-      @products << product
+      products << product
     end
-    @products
+    products
+  end
+
+  def instock_clearance
+    products = products_import
+    products.each { |product|
+      (product.discount_price = product.price.to_i * 0.90) if (product.condition.to_s == "good")
+      (product.discount_price = product.price.to_i * 0.80) if (product.condition.to_s == "average")}
+      products = products.select { |product| product.quantity.to_i > 0 }
+    products
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,6 @@
 class Product
-  attr_accessor :pid, :item, :description, :price, :condition, :dimension_w,
-                :dimension_l, :dimension_h, :img_file, :quantity, :category
-
-  def clearance_price
-    #need to add clearance stuff here
-
-  end
-
-  def discount_price
-  case
-  when self.condition == "good"; discount_price = (self.price.to_f * 0.9)
-  when self.condition == "average"; discount_price = (self.price.to_f * 0.8)
-  else; discount_price = (self.price.to_f * 1.0)
-  end
-  discount_price.to_f
-end
+  attr_accessor :pid, :name, :description, :price, :condition, :dimension_w,
+                :dimension_l, :dimension_h, :img_file, :quantity, :category,
+                :discount_price
 
 end
